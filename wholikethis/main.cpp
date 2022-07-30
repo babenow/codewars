85af07fa57c6ccd5c56c9f0d095b12a11066fea8
Максим Бабенко <babenoff.code@outlook.com>
2022-07-30 15:34:43 +0300

    Who Like This

---
+++ wholikethis/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::string likes(const std::vector<std::string> &names) {
+  std::string result = "no one like this";
+
+  if (names.size() > 0) {
+    size_t len = names.size();
+    std::cout << len << std::endl;
+    if (len < 2) {
+      result = names[0] + " like this";
+    } else if (len == 2) {
+      result = names[0] + " and " + names[1] + " like this";
+    } else if (len == 3) {
+      result = names[0] + ", " + names[1] + " and " + names[2] + " like this";
+    } else {
+      size_t other = len - 2;
+      result =
+          names[0] + ", " + names[1] + " and " + std::to_string((int)other) + " others like this";
+    }
+  }
+  return result;
+}
+
+int main() {
+  std::vector<std::string> names = {"Maks", "Julia", "Father", "Brother"};
+  std::cout << likes(names) << std::endl;
+  return EXIT_SUCCESS;
+}
