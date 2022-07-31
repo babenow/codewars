7e300ed307090750b65b42ef90177c563ead120b
Максим Бабенко <babenoff.code@outlook.com>
2022-07-31 16:51:25 +0300

    Решение задачи Find the odd int

---
+++ include/CodeWars/FindOddInt.hpp
@@ -0,0 +1,9 @@
+#ifndef CODEWARS_FINDODDINT_HPP_
+#define CODEWARS_FINDODDINT_HPP_
+
+#include <vector>
+
+int getCountEntries(const std::vector<int> &numbers, int number);
+int findOdd(const std::vector<int> &numbers);
+
+#endif // CODEWARS_FINDODDINT_HPP_
