22e42e01124cdb2f22cb1c4c58d4a9196987775c
Максим Бабенко <babenoff.code@outlook.com>
2022-08-02 02:38:01 +0300

    Реализация decodeMorse

--- morse_code/morse.cpp
+++ morse_code/morse.cpp
@@ -1,22 +1,36 @@
 #include <CodeWars/MorseCode.hpp>
-#include <iostream>
 #include <regex>
 #include <vector>
 
 std::string decodeMorse(std::string mc) {
-  std::vector<std::string> result;
+  std::vector<std::string> chars;
+  std::string result = "";
   std::regex morseRegex("(([\\.\\-]+)(\\s{1})?)(\\s{2})?");
   std::smatch matches;
   auto begin = mc.cbegin();
   auto end = mc.cend();
-  
+
   while (std::regex_search(begin, end, matches, morseRegex)) {
-    result.push_back(matches[2].str());
+    chars.push_back(matches[2].str());
     if (matches[4].matched) {
-      result.push_back(matches[4].str());
+      chars.push_back(matches[4].str());
     }
     begin += matches.position() + matches.length();
   }
   
-  return "";
+  for (size_t i = 0; i < chars.size(); i++) {
+    if (chars[i] == "  " && i != chars.size() - 1) {
+      result += " ";
+    } else {
+      result += MORSE_CODE[chars[i]];
+    }
+  }for (auto &ch : chars) {
+   if (ch == "  ") {
+    result += " ";
+   } else {
+    result += MORSE_CODE[ch];
+   }
+  }
+  
+  return result;
 }
