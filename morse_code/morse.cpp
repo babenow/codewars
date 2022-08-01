2caab954fc6d0f3fdee244859a4b8ee5276c5bcc
Максим Бабенко <babenoff.code@outlook.com>
2022-08-02 02:56:02 +0300

    Иная реализация decodeMorse

--- morse_code/morse.cpp
+++ morse_code/morse.cpp
@@ -3,34 +3,22 @@
 #include <vector>
 
 std::string decodeMorse(std::string mc) {
-  std::vector<std::string> chars;
+  std::vector<std::string> chars = {};
   std::string result = "";
-  std::regex morseRegex("(([\\.\\-]+)(\\s{1})?)(\\s{2})?");
+  std::regex morseRegex("([^\\s]+|\\s{3})");
   std::smatch matches;
   auto begin = mc.cbegin();
-  auto end = mc.cend();
-
-  while (std::regex_search(begin, end, matches, morseRegex)) {
-    chars.push_back(matches[2].str());
-    if (matches[4].matched) {
-      chars.push_back(matches[4].str());
-    }
+  while (std::regex_search(begin, mc.cend(), matches, morseRegex)) {
+    chars.push_back(matches[0].str());
     begin += matches.position() + matches.length();
   }
-  
-  for (size_t i = 0; i < chars.size(); i++) {
-    if (chars[i] == "  " && i != chars.size() - 1) {
+
+  for (auto ch : chars) {
+    if (ch == "   ") {
       result += " ";
     } else {
-      result += MORSE_CODE[chars[i]];
+      result += MORSE_CODE[ch];
     }
-  }for (auto &ch : chars) {
-   if (ch == "  ") {
-    result += " ";
-   } else {
-    result += MORSE_CODE[ch];
-   }
   }
-  
   return result;
 }
